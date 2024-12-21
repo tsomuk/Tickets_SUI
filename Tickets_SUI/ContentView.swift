43e37e9f3a743926026a3c5a9d861ce7505dd96c
Nikita Tsomuk <123371636+tsomuk@users.noreply.github.com>
2024-12-21 22:34:16 +0200

    Initial Commit

---
+++ Tickets_SUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tickets_SUI
+//
+//  Created by Nikita Tsomuk on 21.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tickets_SUI/Tickets_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tickets_SUIApp.swift
+//  Tickets_SUI
+//
+//  Created by Nikita Tsomuk on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tickets_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
